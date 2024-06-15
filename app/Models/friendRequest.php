d054d90a9c4ff02ff8a9227e435d6f8ed06c07ae
leak vor <vorleak.kun@student.passerellesnumeriques.org>
2024-06-15 08:36:59 +0700

    update main and clean code

--- app/Models/friendRequest.php
+++ app/Models/friendRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class friendRequest extends Model
+class FriendRequest extends Model
 {
     use HasFactory,SoftDeletes;
     protected $fillable = [
@@ -19,7 +19,5 @@ class friendRequest extends Model
     public function user(){
         return $this->belongsTo(User::class,'sender_id');
     }
-
-
 
 }
